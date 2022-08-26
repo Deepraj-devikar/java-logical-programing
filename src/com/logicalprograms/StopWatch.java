a34ab8ac031e144ab188bb588243ab87f0267ee5
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 18:19:16 +0530

    ADDED: difference between stop time and start time

--- src/com/logicalprograms/StopWatch.java
+++ src/com/logicalprograms/StopWatch.java
@@ -1,10 +1,39 @@
 package com.logicalprograms;
 
-public class StopWatch {
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
+public class StopWatch {
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome Stop Watch Program");
-
+		
+		Scanner scanner = new Scanner(System.in);
+		// Start Stop Watch
+		while (true) {
+			System.out.print("Start stop watch by entering 1 : ");
+			int start = scanner.nextInt();
+			if (start == 1) {
+				break;
+			}
+		}
+		LocalDateTime startTime = LocalDateTime.now();
+		
+		// Stop Stop Watch
+		while (true) {
+			System.out.print("Stop stop watch by entering 0 : ");
+			int start = scanner.nextInt();
+			if (start == 0) {
+				break;
+			}
+		}
+		LocalDateTime stopTime = LocalDateTime.now();
+		
+		int hours = stopTime.getHour() - startTime.getHour();
+		int minutes = stopTime.getMinute() - startTime.getMinute();
+		int seconds = stopTime.getSecond() - startTime.getSecond();
+		int nanoSeconds = stopTime.getNano() - startTime.getNano();
+		System.out.println(hours+" : "+minutes+" : "+seconds);
+		scanner.close();
 	}
-
 }
