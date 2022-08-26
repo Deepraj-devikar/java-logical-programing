1fd51cfe38604c2568066609e65dc2b44b85f068
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 12:14:52 +0530

    ADDED: INPUT positive integer number for fibonacci serise

--- src/com/logicalprograms/FibonacciSeries.java
+++ src/com/logicalprograms/FibonacciSeries.java
@@ -1,10 +1,22 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Febonacci Series Program");
 
+		System.out.print("Enter number: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		if (number < 0) {
+			number = -number;
+		}
+		
+		System.out.println("You enterd "+number);
+		
+		scanner.close();
 	}
 
 }
