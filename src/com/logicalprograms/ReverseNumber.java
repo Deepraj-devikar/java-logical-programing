4936c13fb1503289dc066da98450d499e8e3db5c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 13:19:28 +0530

    ADDED: INPUT number to reverse it

--- src/com/logicalprograms/ReverseNumber.java
+++ src/com/logicalprograms/ReverseNumber.java
@@ -1,10 +1,22 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 public class ReverseNumber {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome To Reverse Number Program");
-
+		
+		// INPUT number
+		System.out.print("Enter number : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("You entered "+number);
+		
+		scanner.close();
 	}
 
 }
