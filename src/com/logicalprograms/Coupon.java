f8e99004bc785aff9c768db949cc5ccbf8927d03
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 16:21:28 +0530

    ADDDED: Coupons genration and storage and priting

--- src/com/logicalprograms/Coupon.java
+++ src/com/logicalprograms/Coupon.java
@@ -1,8 +1,20 @@
 package com.logicalprograms;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Coupon {
+	public static ArrayList<Integer> coupons = new ArrayList<Integer>(500);
+	
+	public static void generateCoupon () {
+		while (true) {
+			int crrCoupon = (int) Math.floor(Math.random() * 10000);
+			if (!coupons.contains(crrCoupon) && 1000 <= crrCoupon && crrCoupon <= 9999) {
+				coupons.add(crrCoupon);
+				break;
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Coupon Program");
@@ -13,6 +25,12 @@ public class Coupon {
 		int numberOfCoupons = scanner.nextInt();
 		if (numberOfCoupons < 0) numberOfCoupons = -numberOfCoupons;
 		System.out.println("You have to genrate "+numberOfCoupons+" coupons");
+		
+		// Generate coupon number of times
+		for (int i = 0; i < numberOfCoupons; i++) generateCoupon();
+		
+		System.out.println("Genrated coupons are");
+		for (int i = 0; i < numberOfCoupons; i++) System.out.println(coupons.get(i));
 		
 		scanner.close();
 	}
