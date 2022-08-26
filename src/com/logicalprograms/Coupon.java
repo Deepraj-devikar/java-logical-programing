ea2cae2aaa592b7872b4c0b899e1b72de72b1930
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 13:46:42 +0530

    ADDED: INPUT how many numbers of coupons have to genrate

--- src/com/logicalprograms/Coupon.java
+++ src/com/logicalprograms/Coupon.java
@@ -1,10 +1,20 @@
 package com.logicalprograms;
 
-public class Coupon {
+import java.util.Scanner;
 
+public class Coupon {
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Coupon Program");
 
+		// Input how many coupons to generate
+		System.out.print("How many coupons you have to genrate : ");
+		Scanner scanner = new Scanner(System.in);
+		int numberOfCoupons = scanner.nextInt();
+		if (numberOfCoupons < 0) numberOfCoupons = -numberOfCoupons;
+		System.out.println("You have to genrate "+numberOfCoupons+" coupons");
+		
+		scanner.close();
 	}
 
 }
