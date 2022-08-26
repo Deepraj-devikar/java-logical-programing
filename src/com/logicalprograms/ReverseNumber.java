1220bb7a5f26a4f02299a989bf3833ebd250545c
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 13:25:10 +0530

    ADDED: OUTPUT reverse number

--- src/com/logicalprograms/ReverseNumber.java
+++ src/com/logicalprograms/ReverseNumber.java
@@ -16,6 +16,17 @@ public class ReverseNumber {
 		int number = scanner.nextInt();
 		System.out.println("You entered "+number);
 		
+		// Reverse the number
+		int reverse = 0;
+		int tempNumber = number;
+		while (tempNumber > 0) {
+			reverse = reverse * 10 + tempNumber % 10;
+			tempNumber /= 10;
+		}
+		
+		// Show Reverse number
+		System.out.println(number+"s revers number is "+reverse);
+		
 		scanner.close();
 	}
 
