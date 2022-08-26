35a478dcaa9de5a4fb03079a11b20e4d03f3de67
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 12:35:07 +0530

    ADDED: INPUT positive number to check it is perfect number or not

--- src/com/logicalprograms/PerfectNumber.java
+++ src/com/logicalprograms/PerfectNumber.java
@@ -1,10 +1,22 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Perfect Number Program");
 
+		System.out.print("Enter positive number : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		if (number < 0) {
+			number = -number;
+		}
+		
+		System.out.println("You entered "+number);
+		
+		scanner.close();
 	}
 
 }
