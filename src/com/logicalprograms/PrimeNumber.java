8a3cd95bba3c72e282e5d251e28beef0c46b3d25
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-26 12:57:06 +0530

    ADDED: INPUT positive number greater than 1

--- src/com/logicalprograms/PrimeNumber.java
+++ src/com/logicalprograms/PrimeNumber.java
@@ -1,10 +1,20 @@
 package com.logicalprograms;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Prime Number");
 
+		// INPUT positive number greater than 1
+		System.out.print("Enter positive number grater than 1 : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		if (number < 0) number = -number;
+		System.out.println("You enterd "+number);
+		
+		scanner.close();
 	}
 
 }
